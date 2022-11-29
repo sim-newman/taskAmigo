f80fed63a2b400a14f49fc25c2e0cb558ca0a731
simonnewman <sim.newman@me.com>
2022-11-29 16:42:34 +0000

    add swagger open api specification 3

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 This application will store tasks to a DB, and allo for more effective querying.
 
+swagger ui implemented - http://localhost:8081/swagger-ui/index.html
 
 ## Authors
 
--- src/main/java/com/newman/sim/taskAmigo/TaskAmigoController.java
+++ src/main/java/com/newman/sim/taskAmigo/TaskAmigoController.java
@@ -1,8 +1,7 @@
 package com.newman.sim.taskAmigo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,24 +11,25 @@ public class TaskAmigoController {
     @Autowired
     TaskRepository repository;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){return "TaskAmigo Application is up!";}
 
-    @RequestMapping("/saveTestData")
+    @PostMapping("/saveTestData")
     public String save(){
         repository.save(new Task("Meeting", "1 to 1 Gavin", false, false));
         return "TaskAmigoAppSaved";}
 
-    @RequestMapping("/save")
+    //post for creating new resource and put for updating existing resource
+    @PostMapping("/save")
     public String save(String title, String description, Boolean planned, Boolean completed){
         repository.save(new Task(title, description, planned, completed));
         return "New task " + title + " saved";}
 
-    @RequestMapping("/findAll")
+    @GetMapping("/findAll")
     public Iterable<Task> findAll(){
         return repository.findAll();}
 
-    @RequestMapping("/findAllByTitle")
+    @GetMapping("/findAllByTitle")
     public Iterable<Task> findbyTitle(String title){
         return repository.findByTitle(title) ; }
 
